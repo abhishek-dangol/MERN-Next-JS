ee449d97af823742c32cc654570d518b5e884101
Abhishek Dangol <abhi.dangol@gmail.com>
2021-03-16 11:11:23 -0500

    second commit

---
+++ frontend/pages/_app.js
@@ -0,0 +1,16 @@
+// pages/_app.js
+import React from "react";
+import Head from "next/head";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="viewport-fit=cover" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
