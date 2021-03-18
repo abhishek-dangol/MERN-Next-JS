f955aea162814e8d5f3f1d1b3cf40d0b57b7283b
Abhishek Dangol <abhi.dangol@gmail.com>
2021-03-18 00:04:11 -0500

    added categories

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -44,6 +44,21 @@ const Header = () => {
                 </NavItem>
               </React.Fragment>
             )}
+
+            {isAuth() && isAuth().role === 0 && (
+              <NavItem>
+                <Link href="/user">
+                  <NavLink>{`${isAuth().name}'s Dashboard`}</NavLink>
+                </Link>
+              </NavItem>
+            )}
+            {isAuth() && isAuth().role === 1 && (
+              <NavItem>
+                <Link href="/admin">
+                  <NavLink>{`${isAuth().name}'s Dashboard`}</NavLink>
+                </Link>
+              </NavItem>
+            )}
             {isAuth() && (
               <NavItem>
                 <NavLink
