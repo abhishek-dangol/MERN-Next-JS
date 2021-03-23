5e0c7f54aa2c19061233972ba95a3d9efc654f1f
Abhishek Dangol <abhi.dangol@gmail.com>
2021-03-22 23:21:45 -0500

    Created blog with photo

--- frontend/components/crud/Tag.js
+++ frontend/components/crud/Tag.js
@@ -2,45 +2,45 @@ import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Router from "next/router";
 import { getCookie } from "../../actions/auth";
-import { create, getCategories, removeCategory } from "../../actions/category";
+import { create, getTags, removeTag } from "../../actions/tag";
 
-const Category = () => {
+const Tag = () => {
   const [values, setValues] = useState({
     name: "",
     error: false,
     success: false,
-    categories: [],
+    tags: [],
     removed: false,
     reload: false,
   });
 
-  const { name, error, success, categories, removed, reload } = values;
+  const { name, error, success, tags, removed, reload } = values;
   const token = getCookie("token");
 
   useEffect(() => {
-    loadCategories();
+    loadTags();
   }, [reload]);
 
-  const loadCategories = () => {
-    getCategories().then((data) => {
+  const loadTags = () => {
+    getTags().then((data) => {
       if (data.error) {
         console.log(data.error);
       } else {
-        setValues({ ...values, categories: data });
+        setValues({ ...values, tags: data });
       }
     });
   };
 
-  const showCategories = () => {
-    return categories.map((c, i) => {
+  const showTags = () => {
+    return tags.map((t, i) => {
       return (
         <button
-          onDoubleClick={() => deleteConfirm(c.slug)}
+          onDoubleClick={() => deleteConfirm(t.slug)}
           title="Double click to delete"
           key={i}
           className="btn btn-outline-primary mr-1 ml-1 mt-3"
         >
-          {c.name}
+          {t.name}
         </button>
       );
     });
@@ -49,13 +49,13 @@ const Category = () => {
   const deleteConfirm = (slug) => {
     let answer = window.confirm("Are you sure you want to delete this item?");
     if (answer) {
-      deleteCategory(slug);
+      deleteTag(slug);
     }
   };
 
-  const deleteCategory = (slug) => {
+  const deleteTag = (slug) => {
     //console.log("delete", slug);
-    removeCategory(slug, token).then((data) => {
+    removeTag(slug, token).then((data) => {
       if (data.error) {
         console.log(data.error);
       } else {
@@ -102,19 +102,19 @@ const Category = () => {
 
   const showSuccess = () => {
     if (success) {
-      return <p className="text-success">Category has been created</p>;
+      return <p className="text-success">Tag has been created</p>;
     }
   };
 
   const showError = () => {
     if (error) {
-      return <p className="text-danger">Category already exists</p>;
+      return <p className="text-danger">Tag already exists</p>;
     }
   };
 
   const showRemoved = () => {
     if (removed) {
-      return <p className="text-danger">Category has been removed</p>;
+      return <p className="text-danger">Tag has been removed</p>;
     }
   };
 
@@ -122,7 +122,7 @@ const Category = () => {
     setValues({ ...values, error: false, success: false, removed: "" });
   };
 
-  const newCategoryForm = () => (
+  const newTagForm = () => (
     <form onSubmit={clickSubmit}>
       <div className="form-group">
         <label className="text-muted">Name</label>
@@ -147,11 +147,11 @@ const Category = () => {
       {showError()}
       {showRemoved()}
       <div onMouseMove={mouseMoveHandler}>
-        {newCategoryForm()}
-        {showCategories()}
+        {newTagForm()}
+        {showTags()}
       </div>
     </React.Fragment>
   );
 };
 
-export default Category;
+export default Tag;
